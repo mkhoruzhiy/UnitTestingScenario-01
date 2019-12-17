7e23c8373fd5559833d6e8137242b568afd347e5
KHORUZHIY Maxim <maxim.khoruzhiy@gmail.com>
2019-12-17 10:06:11 +0000

    Service registration cleaned up

--- ConsoleApp/Infrastructure/RegisterServices.cs
+++ ConsoleApp/Infrastructure/RegisterServices.cs
@@ -1,13 +1,9 @@
-﻿using Microsoft.Extensions.Logging;
-using NLog;
-using Rwt.Abstractions.Facades;
+﻿using Rwt.Abstractions.Facades;
 using Rwt.Abstractions.Services;
 using Rwt.Core.Services;
 using Rwt.Persistence.Abstractions;
 using Rwt.Persistence.Repo;
 using Unity;
-using Unity.Injection;
-using Unity.NLog;
 
 namespace ConsoleApp.Infrastructure
 {
@@ -16,12 +12,10 @@ namespace ConsoleApp.Infrastructure
         public static IUnityContainer AddRwtServices(this IUnityContainer @this)
         {
             @this
-                .AddNewExtension<NLogExtension>()
                 .RegisterType<IDataImportService, DataImportService>()
                 .RegisterType<IMessageQueueService, MessageQueueService>()
                 .RegisterType<IRegistryHttpFacade, RegistryHttpFacade>()
                 .RegisterType<IPersonRepository, PersonRepository>()
-                .RegisterType<Microsoft.Extensions.Logging.ILogger>(new InjectionFactory(l => LogManager.GetCurrentClassLogger()));
                 ;
 
             return @this;
