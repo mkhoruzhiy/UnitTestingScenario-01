99189a400ca226ce6b19ee8aa25449a75caafbae
KHORUZHIY Maxim <maxim.khoruzhiy@gmail.com>
2020-01-09 11:04:38 +0100

    Changed test project .net versjion to .NET framework 4.7.2

---
+++ Rwt.Core.Services.UTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Rwt.Core.Services.UTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Rwt.Core.Services.UTest")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("59e6f702-ffb4-4fe4-9db3-50d8cd934254")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
